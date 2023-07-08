e25494cc12b4a26a73318298f7db55f1498e7eed
san <sansamaa0@gmail.com>
2023-07-08 23:08:19 +0300

    nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,10 @@ const Navbar = () => {
   const openDropDown = () => {
     setDropDownMenu(!dropDownMenu)
   }
+  const [userdropDownMenu, setUserDropDownMenu] = useState(false)
+  const openUserDropDown = () => {
+    setUserDropDownMenu(!userdropDownMenu)
+  }
   return (
     <div className='flex bg-black pl-20 pb-4'>
       <div className='logo mt-5 mr-12'>
@@ -56,25 +60,27 @@ const Navbar = () => {
             className='flex ml-1'
           />
         </button>
-        <div
-          className='absolute  z-50 list-none border-none border-amber-300 border-2 rounded bg-black'
-          id='dropdown'
-        >
-          {dropDownMenu && (
-            <ul className=' text-white px-2 text-sms'>
-              <li className='border-b-2 px-6 py-2 border-amber-300'>
-                <button>English</button>
-              </li>
-              <li className='border-b-2  px-6 py-2 outline-8 border-amber-300'>
-                <button> Arabic</button>
-              </li>
-              <li className='border-b-2   px-6 py-2  outline-8 border-amber-300'>
-                <button>German</button>
-              </li>
-              <li className='py-2  px-6'>Fransh</li>
-            </ul>
-          )}
-        </div>
+        {/* <div
+          className=' z-50 list-none border-none border-amber-300 border-2 rounded bg-green-600  '
+          
+        > */}
+        {dropDownMenu && (
+          // <div className='m-5  bg-red-700 ' id='dropdown'>
+          <ul className='fixed text-white text-sms bg-black w-24 p-3 rounded border-2 border-amber-300 border-solid'>
+            <li className='border-b-2 border-amber-300 py-1'>
+              <button>English</button>
+            </li>
+            <li className='border-b-2   outline-8 border-amber-300 py-1 '>
+              <button> Arabic</button>
+            </li>
+            <li className='border-b-2    outline-8 border-amber-300 py-1'>
+              <button>German</button>
+            </li>
+            <li className='py-1'>Fransh</li>
+          </ul>
+          // </div>
+        )}
+        {/* </div> */}
       </div>
       <div className='mt-5 ml-6'>
         <Image
@@ -86,14 +92,32 @@ const Navbar = () => {
         />
       </div>
 
-      <div className='mt-6 ml-9'>
-        <Image
-          src='assets/user.svg'
-          alt='Logo'
-          width={18}
-          height={19}
-          className='flex ml-1'
-        />
+      <div className=' mt-6 ml-9 '>
+        <button onClick={openUserDropDown}>
+          <Image
+            src='assets/user.svg'
+            alt='Logo'
+            width={18}
+            height={19}
+            className='flex ml-1'
+          />
+        </button>
+
+        <div className=''>
+          {userdropDownMenu && (
+            <ul className='fixed text-white  mt-1 right-44 bg-black  text-sms w-24 p-3  rounded border-2 border-amber-300 border-solid'>
+              <li className='border-b-2     border-amber-300 py-1'>
+                <button>User Name</button>
+              </li>
+              <li className='border-b-2   border-amber-300 py-1'>
+                <button>Profile</button>
+              </li>
+              <li className=''>
+                <button>Log out</button>
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
 
       <div className='mt-4 ml-9 text-white'>
